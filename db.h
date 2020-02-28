7075d4250c884c5aac5c15b3d92850c524eeeeb8
AnejL <anej.lekse@gmail.com>
2020-02-28 07:00:44 +0100

    updated config files

--- db.h
+++ db.h
@@ -1,15 +1,20 @@
-// battery dir and wlan dir 
+// number of modules in statusbars that doubles as number of threads
+// if you don't add any of your own modules this shouldn't change
+// caps lock is a special module and should not be counted in here
+#define MODCOUNT		4
+
+// battery dirs
 #define BAT0DIR         "/sys/class/power_supply/BAT0"
 #define BAT1DIR         "/sys/class/power_supply/BAT1"
 #define BAT0            "/sys/class/power_supply/BAT0/capacity"
 #define BAT1            "/sys/class/power_supply/BAT1/capacity"
 
+// wlan and ethernet dirs
 #define WLANDIR         "/sys/class/net/"WLAN
 #define ETHERNETDIR     "/sys/class/net/"ETHERNET
 #define WLANON          "/sys/class/net/"WLAN"/carrier"
 #define ETHERNETON      "/sys/class/net/"ETHERNET"/carrier"
 
-// helper macros
 #define err(mess) { fprintf(stderr,"Error: %s.\n", mess); exit(1); }
 #define quit(mess) { fprintf(stdout,"Quitting: %s.\n", mess); exit(1); }
 
@@ -30,4 +35,5 @@ static Display *dpy;
 // single iteration execution, error supressing and printing to stdout control variables
 int singleiter, noerror, printtostdout;
 
+// address of module format string, used in every module and defined in initvisuals
 char* delimeterformat;
