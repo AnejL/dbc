aef81c9e056df7a87fe2246925eb53d7f7450faa
AnejL <anej.lekse@gmail.com>
2020-03-10 13:02:22 +0100

    fixed configs

--- config.def.h
+++ config.def.h
@@ -2,35 +2,36 @@
 #define DBC_CONFIG_H
 
 	// set to 1 if you want keymap module to show when you have capslock on [ us ] / [ US ] 
-	#define CAPSMODULE		0
-
+    #define CAPSMODULE		0
+	
 	// set to 1 if you want the first module to be your hostname if not, set to 0
-	#define HOSTNAMEMODULE	1
+    #define HOSTNAMEMODULE	1
 
-	// number of seconds between the automatic refresh
-	// set REFRESHINTERVAL to custom value
-	#define REFRESHINTERVAL 1
-
-    // number of batteries to monitor 
-	// set BATTERYCOUNT to  0, 1 or 2
-    #define BATTERYCOUNT   	0
-   
-    // network
+	// network
 	// set names of network devices to monitor (ip a)
-    #define WLAN            "wlp9s0"
-    #define ETHERNET        "eno1"
+    #define WLAN            "wlp3s0"
+    #define ETHERNET        "enp0s31f6"
     
-    // alsa
-	// set PROFILE to match your preferred soundcard profile e.g Master or IEC958
-    #define CARD            "default"
-    #define PROFILE         "Master"
-
-	// delimeter style
-	// 0 => [ mod ]
-	// 1 => | mod |
-	// 2 => < mod <
-	#define STYLE			1
-
-	// turn shorter minimal mode on with 1 here
-	#define MINIMALMODE		0
 #endif //DBC_CONFIG_H
+
+// number of seconds between the automatic refresh
+// set REFRESHINTERVAL to custom value
+static const unsigned int REFRESHINTERVAL 		= 1;
+
+// number of batteries to monitor 
+// set BATTERYCOUNT to  0, 1 or 2
+static const unsigned int BATTERYCOUNT	 		= 2;
+
+// alsa
+// set PROFILE to match your preferred soundcard profile e.g Master or IEC958
+static const char* PROFILE 						= "Master";
+static const char* CARD 						= "default";
+
+// delimeter style
+// 0 => [ mod ]
+// 1 => | mod |
+// 2 => < mod <
+static const unsigned int STYLE 				= 1;
+
+// turn shorter minimal mode on with 1 here
+static const unsigned int MINIMALMODE 			= 0;
